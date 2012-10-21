0dc2c2be7c200cfb2cb26e6a60ab5d4a6b62e962
Michał Sabaciński <sabacs90@gmail.com>
2012-10-21 23:18:39 +0200

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ end
 gem 'jquery-rails'
 
 group :test do
+  gem 'factory_girl_rails', '4.1.0'
   gem 'capybara', '1.1.2'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  resources :users
+
   root to: 'static_pages#home'
   
   match '/help', to: 'static_pages#help'
